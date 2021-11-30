ed7e4637cf318460e635dacd9a63d882362db4d8
Shrikant <shrikant.kotakalure@straviso.com>
2021-11-30 12:43:33 +0530

    Size Classes

---
+++ SizeClass/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SizeClass
+//
+//  Created by shree on 30/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
